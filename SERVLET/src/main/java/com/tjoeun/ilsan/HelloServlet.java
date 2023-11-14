591a41a6f9178bb30879772fc345e453ed9fdfa6
dkdiek <144099638+dkdiek@users.noreply.github.com>
2023-11-14 17:20:27 +0900

    1114 commit

--- SERVLET/src/main/java/com/tjoeun/ilsan/HelloServlet.java
+++ SERVLET/src/main/java/com/tjoeun/ilsan/HelloServlet.java
@@ -14,6 +14,7 @@ public class HelloServlet extends HttpServlet{
 		try {
 			PrintWriter printWriter = res.getWriter();
 			printWriter.write("HelloServlet");
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
--- SERVLET/src/main/java/com/tjoeun/ilsan/calc/CalcServlet.java
+++ SERVLET/src/main/java/com/tjoeun/ilsan/calc/CalcServlet.java
@@ -1,36 +1,64 @@
 package com.tjoeun.ilsan.calc;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// HttpServlet을 상속 받아 doGet 메소드 오버라이딩
+// HttpServlet을 상속 받아 doGet, doPost 메소드 오버라이딩
 public class CalcServlet extends HttpServlet {
-	
-	public void doGet(HttpServletRequest req, HttpServletResponse res) {
-		System.out.println("CalcServlet");
-		//calc.html에서 submit하여 보낸 req에서 num1을 꺼낸다(num1은 calc.html 인풋에 지정되어 있는 name)
-		String num1 = req.getParameter("num1");//getParameter는 값을 String으로 리턴한다
-		String num2 = req.getParameter("num2");
-		// 들어온 값들을 숫자로 변환해서 더하여 변수에 저장
-		int sum = Integer.parseInt(num1) + Integer.parseInt(num2);
-		
-		
-		try {
-			// 인코딩 utf-8로 변경
-			res.setCharacterEncoding("UTF-8");
-			res.setContentType("text/html; charset = UTF-8");
-			
-			//res.getWriter()를 호출하여 PrintWriter 객체를 얻습니다.
-			//이 객체를 사용하여 클라이언트에게 텍스트 기반의 응답을 보낼 수 있습니다.
-			PrintWriter printWriter = res.getWriter();
-			//printWriter.write를 사용하면 CalcServlet에서 클라이언트가 볼수있게 직접 텍스트를 출력할 수 있습니다.
-			printWriter.write("두 수의 합은 " + sum +"입니다");
-		} catch (IOException e) {
-			e.printStackTrace();
+
+	// 포스트 방식
+	public void doPost(HttpServletRequest req, HttpServletResponse res) {
+		System.out.println("CalcServelt - POST");
+		// 계산 결과
+		int result = 0;
+		// 라디오 버튼 이름
+		String operation = req.getParameter("operation");
+
+		if ("sum".equals(operation)) {
+			result = Calculator.getInstance().add(req);
+		} else if ("substract".equals(operation)) {
+			result = Calculator.getInstance().substract(req);
+		} else if ("multiply".equals(operation)) {
+			result = Calculator.getInstance().multiply(req);
+		} else if ("divide".equals(operation)) {
+			result = Calculator.getInstance().divide(req);
+			if (0 == result) {
+				// 나누기는 0으로 나누기 불가 나누기 메소드 실행 후 return으로 아래 소스 스킵
+				CalcDisplay.getInstance().addDivideDisplay(res, result);
+				return;
+			}
 		}
+		// 결과 값 화면 출력
+		CalcDisplay.getInstance().addDisplay(res, result);
+
+	}
+
+	// 겟 방식
+	public void doGet(HttpServletRequest req, HttpServletResponse res) {
+		System.out.println("CalcServlet - Get");
+
+		// 계산 결과
+		int result = 0;
+		// 라디오 버튼 이름
+		String operation = req.getParameter("operation");
+
+		if ("sum".equals(operation)) {
+			result = Calculator.getInstance().add(req);
+		} else if ("substract".equals(operation)) {
+			result = Calculator.getInstance().substract(req);
+		} else if ("multiply".equals(operation)) {
+			result = Calculator.getInstance().multiply(req);
+		} else if ("divide".equals(operation)) {
+			result = Calculator.getInstance().divide(req);
+			if (0 == result) {
+				// 나누기는 0으로 나누기 불가 나누기 메소드 실행 후 return으로 아래 소스 스킵
+				CalcDisplay.getInstance().addDivideDisplay(res, result);
+				return;
+			}
+		}
+		// 결과 값 화면 출력
+		CalcDisplay.getInstance().addDisplay(res, result);
+
 	}
 }
